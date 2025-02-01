79d2239d35c6104a2007978b3ab8efd80b446f95
for-the-zero <ourmicroid@outlook.com>
2025-02-01 17:42:28 +0800

    啊啊啊啊啊啊啊啊

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
 mdui.setColorScheme('#DD0066');
+
+var uid_list = [];
+
+const e_uidadd = $('.uid-add');
+const e_uidimp = $('.uid-import');
+const e_uidexp = $('.uid-export');
+
+e_uidadd.on('click',()=>{
+    mdui.prompt({
+        headline: "添加UID",
+        confirmText: "添加",
+        cancelText: "取消",
+        onConfirm: (value) => {
+            if(value.length>0 && /^\d+$/.test(value)){
+                value = parseInt(value);
+                if(uid_list.indexOf(value)<0){
+                    uid_list.push(value);
+                    mdui.snackbar({message: "添加成功",closeable: true});
+                    reflash_uid_list();
+                }else{
+                    mdui.snackbar({message: "UID已存在",closeable: true});
+                }
+            }else{
+                mdui.snackbar({message: "请输入正确的UID",closeable: true});
+            };
+        }
+    });
+});
+e_uidimp.on('click',()=>{
+    mdui.prompt({
+        headline: "导入列表",
+        description: "JSON或url导入",
+        confirmText: "导入",
+        cancelText: "取消",
+        onConfirm: (value) => {
+            if(value.startsWith("[")){
+                try{
+                    var data = JSON.parse(value);
+                    if(Array.isArray(data)){
+                        uid_list = data;
+                        mdui.snackbar({message: "导入成功",closeable: true});
+                        reflash_uid_list();
+                    }else{
+                        mdui.snackbar({message: "导入失败",closeable: true});
+                    };
+                }catch(e){
+                    mdui.snackbar({message: "导入失败",closeable: true});
+                };
+            } else {
+                $.ajax({
+                    url: value,
+                    type: "GET",
+                    dataType: "text",
+                    success: function(data){
+                        try{
+                            var data = JSON.parse(data);
+                            if(Array.isArray(data)){
+                                uid_list = data;
+                                mdui.snackbar({message: "导入成功",closeable: true});
+                                reflash_uid_list();
+                            }else{
+                                mdui.snackbar({message: "导入失败",closeable: true});
+                            };
+                        }catch(e){
+                            mdui.snackbar({message: "导入失败",closeable: true});
+                            console.error(e);
+                        };
+                    },
+                    error: function(xhr, status, error) {
+                        mdui.snackbar({message: "导入失败",closeable: true});
+                        console.error(xhr, status, error);
+                    },
+                });
+            };
+        },
+    });
+});
+e_uidexp.on('click',()=>{
+    navigator.clipboard.writeText(JSON.stringify(uid_list));
+    mdui.snackbar({message: "导出成功",closeable: true});
+});
+
+function reflash_uid_list(){
+    //TODO:
+};
